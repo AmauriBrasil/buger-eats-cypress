a28b89ceec0ec45a45eada9c43fc88af589562a0
AmauriBrasil <amauribrasil80@gmail.com>
2022-02-02 11:54:51 -0300

    Deploy para master
    Toda as informações da Refatoração está contida na branch teste

--- cypress/integration/Signup.spec.js
+++ cypress/integration/Signup.spec.js
@@ -21,11 +21,11 @@ describe('Cadastro', () => {
         signupPage.modalContentShouldBe(expectedMessage)
     })
 
-    it('Cpf inválido', function () {
+    it('Validar cpf inválido', function () {
 
         var deliver = signupFactory.deliver()
 
-        deliver.cpf = '00000014ABC'
+        deliver.cpf = '000000141aa'
 
         signupPage.go()
         signupPage.fillForm(deliver)
@@ -33,7 +33,7 @@ describe('Cadastro', () => {
         signupPage.alertMessageShouldBe('Oops! CPF inválido')     
     })
 
-    it('Email inválido', function () {
+    it('Validar email inválido', function () {
 
         var deliver = signupFactory.deliver()
 
@@ -43,5 +43,30 @@ describe('Cadastro', () => {
         signupPage.fillForm(deliver)
         signupPage.submit()
         signupPage.alertMessageShouldBe('Oops! Email com formato inválido.')            
+    })
+
+    context('Verificar e validar campos obrigatórios', function () {
+        
+        const messages = [
+            { field: 'name', output: 'É necessário informar o nome' },
+            { field: 'cpf', output: 'É necessário informar o CPF' },
+            { field: 'email', output: 'É necessário informar o email' },
+            { field: 'postalcode', output: 'É necessário informar o CEP' },
+            { field: 'number', output: 'É necessário informar o número do endereço' },
+            { field: 'delivery_method', output: 'Selecione o método de entrega' },
+            { field: 'cnh', output: 'Adicione uma foto da sua CNH' },
+        ]
+
+        before(function(){
+            signupPage.go()
+            signupPage.submit()
+        })
+        
+        messages.forEach(function(msg){
+            it(`${msg.field} is required`, function(){
+                signupPage.alertMessageShouldBe(msg.output)
+            })
+        })
+
     })
 })
